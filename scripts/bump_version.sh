cde73de862e29d078b75a23fb1c80df338a87749
Alexandr Moroz <alexandrmoroz@gmail.com>
2021-10-31 00:19:39 +0300

    минимальная версия ios 14, убрал ошибку из скрипта публикации: в этом приложении нет расширений

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -5,12 +5,7 @@ sed -i.bak "s/^\(version:.*[.]\).*$/\1$build_number+$build_number/" pubspec.yaml
 
 version=$(grep 'version: ' pubspec.yaml | sed "s/^[^0-9]*\(.*[.]\).*/\1$build_number/")
 
-# extension
-extPlistPath="ios/UsageWidgets/Info.plist"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" $extPlistPath
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" $extPlistPath
-
-git commit -m "Bump version to $version" pubspec.yaml $extPlistPath
+git commit -m "Bump version to $version" pubspec.yaml
 git tag "$version"
 git push
 
